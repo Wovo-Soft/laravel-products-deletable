e620a184e286dae5f8df9697457f36147bcfb8f5
Narayan Adhikary <narayanadhikary24@gmail.com>
2020-04-28 22:01:06 +0600

    ProductsFactory Fixed

--- database/factories/ProductsFactory.php
+++ database/factories/ProductsFactory.php
@@ -8,8 +8,8 @@ $factory->define(config('laravel-products.products.model'), function (Faker $fak
     return [
         'name' => $faker->name,
         'upc' => uniqid(),
-        'type' => config('laravel-products.product_types')[random_int(0, count(config('laravel-products.product_types')) - 1)],
-        'unit' => config('laravel-products.product_units')[random_int(0, count(config('laravel-products.product_units')) - 1)],
+        'type' => config('laravel-products.products.types')[random_int(0, count(config('laravel-products.products.types')) - 1)],
+        'unit' => config('laravel-products.products.units')[random_int(0, count(config('laravel-products.products.units')) - 1)],
 //        'image' => $faker->imageUrl(),
         'image' => 'https://picsum.photos/400/600/?image=' . random_int(500, 1000),
         'cost' => $faker->randomFloat(2, 50, 150),
